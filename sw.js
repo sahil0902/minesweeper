3a548ccb1c23356373338e3a51661cf6c4c72b86
Muhammad.Sahil <muhammadsahil757@gmail.com>
2024-01-20 16:15:14 +0000

    Added offline support for IPhone

--- sw.js
+++ sw.js
@@ -3,12 +3,16 @@ const cacheName = 'v1';
 // Cache assets
 const assetsToCache = [
   '/',
-  '/index.html',
-  '/script.js',
-  '/style.css',
-  '/images/logo.jpeg',
-  '/images/bomb.png',
-  '/audios/*'
+  'index.html',
+  'script.js',
+  'style.css',
+  'images/logo.jpeg',
+  'images/bomb.png',
+  'audios/lose.mp3',
+  'audios/loseaudio.mp3',
+  'audios/start.wav',
+  'audios/tap.wav',
+  'audios/win.mp3'
 ];
 
 self.addEventListener('install', e => {
